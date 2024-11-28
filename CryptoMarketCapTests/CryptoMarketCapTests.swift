df7d68fc5cb1bae47ea50509cf30b3c1d376e158
Hoorad <hooradramezani@gmail.com>
2024-11-28 14:23:06 +0330

    Initial Commit

---
+++ CryptoMarketCapTests/CryptoMarketCapTests.swift
@@ -0,0 +1,16 @@
+//
+//  CryptoMarketCapTests.swift
+//  CryptoMarketCapTests
+//
+//  Created by Hoorad on 11/28/24.
+//
+
+import Testing
+
+struct CryptoMarketCapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
